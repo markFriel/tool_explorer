37ca55a81ea95bfcf9154fa945c8ea4ea25f10af
Mark Friel <31072432+markFriel@users.noreply.github.com>
2025-03-22 09:08:23 +0100

    build:
    Signed-off-by: Mark Friel <31072432+markFriel@users.noreply.github.com>

--- src/learning_pipline.py
+++ src/learning_pipline.py
@@ -12,4 +12,5 @@ if __name__ == "__main__":
     luigi.build(DataTasks(), local_scheduler=True, workers=1)
     luigi.build(FeatureTasks(), local_scheduler=True, workers=1)
     luigi.build(ModelTasks(), local_scheduler=True, workers=1)
+    
     luigi.build(EvaluationTasks(), local_scheduler=True, workers=1)
